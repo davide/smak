24943b305cab6d5372ce659d1869f5cbad669ec1
Hunter Morris <huntermorris@gmail.com>
2009-02-20 14:12:28 +0000

    Added calendar types for dialyzer

--- include/smak.hrl
+++ include/smak.hrl
@@ -40,4 +40,25 @@
           fragment=[]
          }).
 
+%%----------------------------------------------------------------------
+%% Calendar types (from calendar.erl in OTP R12B5)
+%%----------------------------------------------------------------------
+
+-type year()     :: non_neg_integer().
+-type year1970() :: 1970..10000.	% should probably be 1970..
+-type month()    :: 1..12.
+-type day()      :: 1..31.
+-type hour()     :: 0..23.
+-type minute()   :: 0..59.
+-type second()   :: 0..59.
+-type daynum()   :: 1..7.
+-type ldom()     :: 28 | 29 | 30 | 31. % last day of month
+
+-type t_now()    :: {non_neg_integer(),non_neg_integer(),non_neg_integer()}.
+
+-type t_date()         :: {year(),month(),day()}.
+-type t_time()         :: {hour(),minute(),second()}.
+-type t_datetime()     :: {t_date(),t_time()}.
+-type t_datetime1970() :: {{year1970(),month(),day()},t_time()}.
+
 -endif.
