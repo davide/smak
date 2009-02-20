62b94740d4bc084d95e3e0525ec9688eb83eada4
Hunter Morris <huntermorris@gmail.com>
2009-02-20 14:25:36 +0000

    Fixed dialyzer warnings

--- src/smak_sn_cookie.erl
+++ src/smak_sn_cookie.erl
@@ -21,22 +21,20 @@
 -author('Hunter Morris <hunter.morris@smarkets.com>').
 
 -define(MAX_SIZE, 4096). %% Most browsers only allow 4K of cookies
--define(MAX_SIZE_REPR, ?MAX_SIZE / 2). %% Assume a 2-character representation of each byte in the final cookie.
+-define(MAX_SIZE_REPR, 2048). %% Assume a 2-character representation of each byte in the final cookie.
 
 -export([encode/2, encode/3, decode/3]).
 
 %% @spec encode(Key::binary(), Data::binary()) -> binary() | {error, Reason::atom()}
 %% @doc Encodes and signs the data using the specified secret key.
 %% @see encode/3
--spec(encode/2 :: (binary(), binary()) -> binary() | {error, atom()}).
-             
+-spec encode(binary(), binary()) -> binary() | {error, atom()}.
 encode(Key, Data) ->
     encode(Key, Data, ?MAX_SIZE_REPR).
 
 %% @spec encode(Key::binary(), Data::binary(), MaxSize::integer()) -> binary() | {error, Reason::atom()}
 %% @doc Encodes and signs the data using the specified secret key and provides an error if it exceeds the maximum size.
--spec(encode/3 :: (binary(), binary(), integer()) -> binary() | {error, atom()}).
-
+-spec encode(binary(), binary(), integer()) -> binary() | {error, atom()}.
 encode(Key, Data, MaxSize) when MaxSize > 0, is_binary(Data), is_binary(Key) ->
     IV = crypto:rand_bytes(16),
     DataCompressed = zlib:zip(Data),
@@ -53,8 +51,7 @@ encode(Key, Data, MaxSize) when MaxSize > 0, is_binary(Data), is_binary(Key) ->
     
 %% @spec decode(Key::binary(), Cookie::binary(), Timeout::integer()) -> binary() | {error, Reason::atom()}
 %% @doc Checks the signature and timeout and decrypts the cookie if it is valid.
--spec(decode/3 :: (binary(), binary(), integer()) -> binary() | {error, atom()}).
-
+-spec decode(binary(), binary(), integer()) -> binary() | {error, atom()}.
 decode(Key, <<HMACSignature:20/binary, Timestamp:48/integer, IV:16/binary, DataCrypted/bits>>, Timeout) ->
     MinimumAge = smak_calendar:now_utc_ts_ms() - Timeout,
     case crypto:sha_mac(Key, <<Timestamp:48/integer, IV:16/binary, DataCrypted/bits>>) of
